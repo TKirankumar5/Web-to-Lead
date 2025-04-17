7c80a0aa860f93cf6b20ff2fc3e3d21604040cc0
TKirankumar5 <kirankumart873@gmail.com>
2025-04-17 11:59:19 +0530

    Making Recaptcha required

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function beforesubmit(){
-
- let outputdate = document.querySelector(".outputdate");   
+function beforesubmit(event){
+if(captchaChecked){
+    let outputdate = document.querySelector(".outputdate");   
  let inputdate = document.querySelector(".inputdate");
 console.log('inputdate',inputdate.value ); //string --Date (en_IN)
 
@@ -10,6 +10,18 @@ outputdate.value = formattedDate;
 
 console.log('outputdate', outputdate.value);
 
+}else{
+    alert("Please check the reCaptcha box");
+    event.preventDefault();
+}
+ 
 }
 
 function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+
+
+let captchaChecked = false;
+
+function captchasuccess(){
+    captchaChecked = true;
+}

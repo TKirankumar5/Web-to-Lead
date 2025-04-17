58f1e7e6321d67e86e14152bb56f3e76151d5262
TKirankumar5 <kirankumart873@gmail.com>
2025-04-17 11:50:47 +0530

    Added Recaptcha

--- script.js
+++ script.js
@@ -11,3 +11,5 @@ outputdate.value = formattedDate;
 console.log('outputdate', outputdate.value);
 
 }
+
+function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
